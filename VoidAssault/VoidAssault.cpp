7ede5dad0fb92d99c314b79165324a12a2d82548
Ёж Соник <g.mens.hl@gmail.com>
2025-12-07 22:59:00 +0300

    Добавьте файлы проекта.

---
+++ VoidAssault/VoidAssault.cpp
@@ -0,0 +1,12 @@
+﻿// VoidAssault.cpp: определяет точку входа для приложения.
+//
+
+#include "VoidAssault.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
