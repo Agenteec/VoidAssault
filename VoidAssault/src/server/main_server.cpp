246d9b1ce0b1870b84d9c73d6c2d5468f29c808b
Ёж Соник <g.mens.hl@gmail.com>
2025-12-13 04:49:21 +0300

    Переписан сервер для systemd

--- VoidAssault/src/server/main_server.cpp
+++ VoidAssault/src/server/main_server.cpp
@@ -1,6 +1,17 @@
 ﻿#include "../engine/ServerHost.h"
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <csignal>
+
+std::atomic<bool> keepRunning{ true };
+
+void SignalHandler(int signum) {
+    std::cout << "\nSignal " << signum << " received. Stopping server...\n";
+    keepRunning = false;
+}
 
 int main() {
     srand(time(NULL));
@@ -8,32 +19,30 @@ int main() {
         std::cerr << "An error occurred while initializing ENet.\n";
         return 1;
     }
-//    std::string configPath = "./";
-//
-//#if defined(ANDROID) || defined(PLATFORM_ANDROID)
-//
-//    configPath = GetApplicationDirectory();
-//#endif
-//
-//    ConfigManager::Initialize(configPath);
 
-
-    
 #ifdef WIN32
     InitWindow(100, 100, "ServerHeadless");
     SetWindowState(FLAG_WINDOW_HIDDEN);
-#endif // WIN32
-
-
+#else
+    signal(SIGINT, SignalHandler);
+    signal(SIGTERM, SignalHandler);
+#endif
 
     ServerHost server;
     if (server.Start(7777)) {
-        std::cout << "Dedicated Server started on port 7777. Type 'quit' to stop.\n";
+        std::cout << "Dedicated Server started on port 7777.\n";
 
+#ifdef WIN32
+        std::cout << "Type 'quit' to stop.\n";
         std::string cmd;
         while (std::cin >> cmd) {
             if (cmd == "quit") break;
         }
+#else
+        while (keepRunning) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+#endif
 
         server.Stop();
     }
